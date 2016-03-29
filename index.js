69c8e2c242da6dcbc7b5848879c03f720eb64daf
avikj <avikjain12345@gmail.com>
2016-03-28 21:46:09 -0700

    Message self in private chat to run commands, bug causes it to be called 4 times

--- index.js
+++ index.js
@@ -6,26 +6,41 @@ var loginInfo = JSON.parse(fs.readFileSync("secretData.json"));
 var cds = [];
 login(loginInfo, function callback (err, api) {
     if(err) return console.error(err);
+    api.setOptions({selfListen: true});
 
     api.listen(function callback(err, message) {
+    	//if(message.senderID != message.threadID) // do not accept messages from group chats
+    	  //  return; 
+        if(message.senderID != api.getCurrentUserID()) // do not accept messages from other people
+    	    return;
+        if(message.body.startsWith("@fbterm"))	// do not accept messages that were sent by the bot
+    	    return;
     	if(message.body.indexOf("cd") != -1){
     		console.log(message.body);
     		cds.push(message.body+" && ");
+            var command = "";
+            for(var i = 0; i < cds.length; i++)
+                command+=cds[i];
+            command+="pwd";
+            exec(command, function(error, stdout, stderr){
+                api.sendMessage("@fbterm\n"+stdout, message.threadID);
+            });
+            return;
     	}
-    	else{
-    		var command = "";
-    		for(var i = 0; i < cds.length; i++)
-    			command+=cds[i];
-    		command+=message.body;
-	    	console.log(command);
+        	
+    	var command = "";
+    	for(var i = 0; i < cds.length; i++)
+    		command+=cds[i];
+    	command+=message.body;
+    	console.log(command);
 
-	    	exec(command, function(error, stdout, stderr){
-	    		
-	    		if(error)
-	    			api.sendMessage("@fbterm\n"+error.toString(), message.threadID);
-	    		else
-	    			api.sendMessage("@fbterm\n"+stdout+"\n"+stderr, message.threadID);
-	    	});
-	    }
+    	exec(command, function(error, stdout, stderr){
+    		
+    		if(error)
+    			api.sendMessage("@fbterm\n"+error.toString(), message.threadID);
+    		else
+    			api.sendMessage("@fbterm\n"+stdout+"\n"+stderr, message.threadID);
+    	});
+    	    
     });
 });

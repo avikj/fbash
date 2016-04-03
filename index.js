b55b6889c95ad3da5e9a4b03969340d075b4ee73
avikj <avikjain12345@gmail.com>
2016-04-02 22:46:52 -0700

    Improved cd handling using cwd option in exec()

--- index.js
+++ index.js
@@ -6,32 +6,70 @@ var loginInfo = JSON.parse(fs.readFileSync("secretData.json"));
 var cds = [];
 var lastDate = Date.now();
 var lastMessage = "";
+
+var directory = __dirname;
+console.log(__dirname);
 login(loginInfo, function callback (err, api) {
+
     if(err) return console.error(err);
+
     api.setOptions({selfListen: true});
     api.setOptions({disableDelta: true});
+
     api.listen(function callback(err, message) {
+
     	if(message.senderID != message.threadID) // do not accept messages from group chats
-    	    return; 
+    	    if(message.body.startsWith("/fbterm"))
+                message.body = message.body.substring(7);
+            else
+                return;
+
         if(message.senderID != api.getCurrentUserID()) // do not accept messages from other people
     	    return;
+
         if(message.body.startsWith("@fbterm"))	// do not accept messages that were sent by the bot
     	    return;
+
     	if(Date.now()-lastDate < 300 && lastMessage == message.body)	// prevent duplicate calls
 	    return;
     	lastDate = Date.now();
 	   lastMessage = message.body;
     	console.log("\nexecuting command @ "+Date.now());
 
-        if(message.body.trim() == "cls" || message.body.trim() == "clear"){
+        if(message.body.trim() == "cls" || message.body.trim() == "clear"){     // deletes thread (clears messages)
             api.sendMessage("@fbterm\nReload page to finish clearing thread.", message.threadID, function(err, messageInfo){
                 api.deleteThread(message.threadID);
+                api.sendMessage("@fbterm :)", message.threadID);
             });
             return;
         }
 
-    	if(message.body.indexOf("cd") != -1){
-    		console.log(message.body);
+        /* if(message.body.toLowerCase().startsWith("sendfile ")){
+            var filename = message.body.substring(9).trim();
+            console.log("Attempting to send file: "+filename);
+            fs.stat(__dirname+"/"+filename, function(err, stat) { //check if file exists
+                if(err == null) {
+                    api.sendMessage({body: "@fbterm "+filename, attachment: fs.createReadStream(__dirname+"/"+filename)}, message.threadID);
+                    console.log('File exists');
+                } else if(err.code == 'ENOENT') {
+                    api.sendMessage("@fbterm ERR:\nFile "+filename+" not found.", message.threadID);
+                } else {
+                    console.log('Some other error: ', err.code);
+                }
+            });
+            return;
+        }*/
+
+    	if(message.body.trim().startsWith("cd")){
+            var relativeDir = message.body.trim().substring(2).trim();
+            if(relativeDir == "..")
+                directory = directory.substring(0, directory.lastIndexOf("\\"));
+            else if(relativeDir != ".")
+                directory+="\\"+relativeDir;
+
+            console.log("cwd: "+directory);
+            return;
+    		/*console.log(message.body);
     		cds.push(message.body+" && ");
             var command = "";
             for(var i = 0; i < cds.length; i++)
@@ -40,7 +78,7 @@ login(loginInfo, function callback (err, api) {
             exec(command, function(error, stdout, stderr){
                 api.sendMessage("@fbterm\n"+stdout, message.threadID);
             });
-            return;
+            return;*/
     	}
         	
     	var command = "";
@@ -49,8 +87,10 @@ login(loginInfo, function callback (err, api) {
     	command+=message.body;
     	console.log(command);
 
-    	exec(command, function(error, stdout, stderr){
-
+    	exec(command, {
+            cwd: directory
+        }, function(error, stdout, stderr){
+            // stdout = stdout.replace(/\./g, ",");     // replaces periods with commas to prevent fb bot detection
     		if(error)
     			api.sendMessage("@fbterm ERR:\n"+error, message.threadID);
     		else{

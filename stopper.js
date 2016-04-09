b1eef99e1983daa269898c2c3d75e89ef148d0a1
avikj <avikjain12345@gmail.com>
2016-04-08 22:19:43 -0700

    Prevent err caused by null data when no forever process are running

--- stopper.js
+++ stopper.js
@@ -2,6 +2,7 @@
 var forever = require('forever');
 
 forever.list(false, function(err, data){
+	if(data == null) return;
 	for(var i = 0; i < data.length; i++){
 		if(data[i].uid == "fbash")
 			forever.stop(i);
